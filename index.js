c20b3ab57e927f77800b356b24cb22e1a31cbf32
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-12-13 20:41:44 +0600

    create orders api for get specific data from mongoDb

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services')
+        const orderCollection = client.db('geniusCar').collection('orders')
 
-        // read (find) Multiple data from mongoDb
+        // read,get (find,get) Multiple data from mongoDb
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
@@ -28,12 +29,38 @@ async function run() {
             res.send(services)
         })
 
-        //read (dynamic data/singleData) from mongoDb
+        //read, get (dynamic data/singleData) from mongoDb
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const service = await serviceCollection.findOne(query)
             res.send(service)
+        })
+
+        //orders API for get specific  data from mongoDb !
+        // v6
+        app.get('/orders', async (req, res) => {
+            //url link ta server display side er
+
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray()
+            res.send(orders)
+        })
+
+        // orders API for create ( insert, send ) a data to mongoDb
+        app.post('/orders', async (req, res) => {
+            //url link ta Client side er
+            const order = req.body;
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
         })
 
     }
